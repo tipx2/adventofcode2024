459063feeaed75b4f34c915867ff7505ed3e1a98
tipx2 <tipx2gaming@gmail.com>
2024-11-30 23:35:43 +0000

    added user-agent field as per https://old.reddit.com/r/adventofcode/comments/z9dhtd/please_include_your_contact_info_in_the_useragent/

--- importer.py
+++ importer.py
@@ -2,6 +2,10 @@ import requests
 import os.path
 from datetime import datetime
 
+headers = {
+  "User-Agent" : "https://github.com/tipx2/adventofcode2024 by tipx2gaming [AT] gmail [DOT] com" 
+}
+
 if not os.path.isfile("session.txt"):
   print("Please put session token in a file named 'session.txt' in the same folder as this script")
   exit(0)
@@ -11,7 +15,7 @@ def r(n):
   s = requests.session()
   s.cookies.set("session", open("session.txt", "r").read().strip())
   
-  response = s.get("https://adventofcode.com/2024/day/" + n + "/input")
+  response = s.get("https://adventofcode.com/2024/day/" + n + "/input", headers=headers)
   data = response.text
   
   w_file = "day" + n + "/input" + n + ".txt"
