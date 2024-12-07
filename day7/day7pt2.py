2c77c64a4018a0ea73c3893f831a53058c617fa6
tipx2 <tipx2gaming@gmail.com>
2024-12-07 09:02:37 +0000

    day 7

---
+++ day7/day7pt2.py
@@ -0,0 +1,29 @@
+from itertools import product
+with open("day7/input7.txt") as f:
+  lines = f.readlines()
+
+over_total = 0
+for line in lines:
+  
+  result, eqn = line.split(": ")
+  result = int(result)
+  eqn = [int(x) for x in eqn.split(" ")]
+  
+  operators = list(product("+*|", repeat=len(eqn) - 1))
+  
+  for op in operators:
+    total = eqn[0]
+    
+    for x in range(len(op)):
+      if op[x] == "+":
+        total += eqn[x + 1]
+      elif op[x] == "*":
+        total *= eqn[x + 1]
+      elif op[x] == "|":
+        total = int(str(total) + str(eqn[x + 1]))
+    
+    if total == result:
+      over_total += result
+      break
+
+print(over_total)
