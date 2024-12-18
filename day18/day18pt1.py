5344242f7bb9d9b85d0ab089a6095538e4108b84
tipx2 <tipx2gaming@gmail.com>
2024-12-18 14:08:49 +0000

    day 18

---
+++ day18/day18pt1.py
@@ -0,0 +1,30 @@
+import heapq
+with open("day18/input18.txt") as f:
+  lines = [tuple(int(z) for z in x.strip().split(",")) for x in f.readlines()]
+
+end = (70, 70)
+
+walls = []
+for i in range(1024):
+  walls.append(lines[i])
+
+directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+check = [(0,0,0)] # weight, x, y
+visited = set()
+
+while check:
+  weight, currx, curry = heapq.heappop(check)
+  
+  if (currx, curry) == end:
+    print(weight)
+    break
+  
+  if (currx, curry) in visited:
+    continue
+  visited.add((currx, curry))
+  
+  for d in directions:
+    t_dir = (weight + 1, currx + d[0], curry + d[1])
+    if (t_dir[1], t_dir[2]) not in walls and 0 <= t_dir[1] <= 70 and 0 <= t_dir[2] <= 70:
+      heapq.heappush(check, t_dir)
